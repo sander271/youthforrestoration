ba3f388f793eba7e92b53ee308a57f5f49052a72
Aston Sanders <sanders271@gmail.com>
2015-04-15 16:07:44 -0400

    edit php files

--- web/index.php
+++ web/index.php
@@ -31,12 +31,12 @@
     <div id="navbarspace"></div>
     <div id="navbar">
         <ul id="menubar" class="container">
-            <li class="nav-item sc-content-editable1" onclick=home()>Home</li>
-            <li class="nav-item sc-content-editable2" onclick=aboutpage()>About Us</li>
-            <li class="nav-item sc-content-editable3" onclick="projectpage()">Projects</li>
-            <li class="nav-item sc-content-editable4" onclick="newspage()">News</li>
-            <li class="nav-item sc-content-editable5" onclick="contactpage()">Contact</li>
-            <li class="nav-item sc-content-editable6" onclick="donatepage()">Donate</li>
+            <li class="nav-item" onclick=home()>Home</li>
+            <li class="nav-item" onclick=aboutpage()>About Us</li>
+            <li class="nav-item" onclick="projectpage()">Projects</li>
+            <li class="nav-item" onclick="newspage()">News</li>
+            <li class="nav-item" onclick="contactpage()">Contact</li>
+            <li class="nav-item" onclick="donatepage()">Donate</li>
         </ul>
     </div>
     <div id="body-wrapper">
