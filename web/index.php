c08122454a3c6d4778a035320966054a0a2aedb3
Aston Sanders <sanders271@gmail.com>
2015-04-15 13:41:49 -0400

    edit php web page file

--- web/index.php
+++ web/index.php
@@ -29,36 +29,42 @@
 <body id="body" >
 <div id="contentwrapper">
     <div id="navbarspace"></div>
-    <div id="navbar" class="sc-content-editable">
-        <ul id="menubar" class="container">
-            <li class="nav-item" onclick=home()>Home</li>
-            <li class="nav-item" onclick=aboutpage()>About Us</li>
-            <li class="nav-item" onclick="projectpage()">Projects</li>
-            <li class="nav-item" onclick="newspage()">News</li>
-            <li class="nav-item" onclick="contactpage()">Contact</li>
-            <li class="nav-item" onclick="donatepage()">Donate</li>
-        </ul>
+    <div class="sc-content-editable">
+        <div id="navbar">
+            <ul id="menubar" class="container">
+                <li class="nav-item" onclick=home()>Home</li>
+                <li class="nav-item" onclick=aboutpage()>About Us</li>
+                <li class="nav-item" onclick="projectpage()">Projects</li>
+                <li class="nav-item" onclick="newspage()">News</li>
+                <li class="nav-item" onclick="contactpage()">Contact</li>
+                <li class="nav-item" onclick="donatepage()">Donate</li>
+            </ul>
+        </div>
     </div>
     <div id="body-wrapper">
         <div id="homepagespace"></div>
-        <div id="homepagecontent" class="container" class="sc-content-editable1">
-            <h2 class="text">Youth for Restoration is a youth-led, nonprofit  organization located in the Hudson Valley, New York.
-                We are dedicated to preserving and restoring local historic sites. ​</h2>
+        <div class="sc-content-editable1">
+            <div id="homepagecontent" class="container">
+                <h2 class="text">Youth for Restoration is a youth-led, nonprofit  organization located in the Hudson Valley, New York.
+                    We are dedicated to preserving and restoring local historic sites. ​</h2>
+            </div>
         </div>
     </div>
     <div class="clearfooter"></div>
 </div>
-<div id="bottombar" class="container" class="sc-content-editable2">
-    <a href="https://www.facebook.com/youthforrestoration" target="_blank">
-        <img class="thumbnail" src = "https://www.facebook.com/images/fb_icon_325x325.png" />
-    </a>
-    <a href="https://twitter.com/Y_F_R_" target="_blank">
-        <img class="thumbnail" src = "http://www.buggytours.nl/img/Twitterbutton.png" />
-    </a>
-    <a href="http://youthforrestoration.blogspot.com/" target="_blank">
-        <img class="thumbnail" src = "http://icons.iconarchive.com/icons/marcus-roberto/google-play/512/Blogger-icon.png" />
-    </a>
-    <h3 class="text">Copyright@2014 by Youth For Restoration, All rights reserved</h3>
+<div class="sc-content-editable2">
+    <div id="bottombar" class="container">
+        <a href="https://www.facebook.com/youthforrestoration" target="_blank">
+            <img class="thumbnail" src = "https://www.facebook.com/images/fb_icon_325x325.png" />
+        </a>
+        <a href="https://twitter.com/Y_F_R_" target="_blank">
+            <img class="thumbnail" src = "http://www.buggytours.nl/img/Twitterbutton.png" />
+        </a>
+        <a href="http://youthforrestoration.blogspot.com/" target="_blank">
+            <img class="thumbnail" src = "http://icons.iconarchive.com/icons/marcus-roberto/google-play/512/Blogger-icon.png" />
+        </a>
+        <h3 class="text">Copyright@2014 by Youth For Restoration, All rights reserved</h3>
+    </div>
 </div>
 </body>
 </html>
