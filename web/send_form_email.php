f75b7125619b614dd43dfbb1d0aa10483508d22b
Aston Sanders <sanders271@gmail.com>
2015-04-27 16:35:15 -0400

    test email function

--- web/send_form_email.php
+++ web/send_form_email.php
@@ -1,106 +1,12 @@
 <?php
 require 'vendor/autoload.php';
-//if(isset($_POST['email'])) {
-//    // EDIT THE 2 LINES BELOW AS REQUIRED
-//    $email_to = "sanders271@gmail.com";
-//    $email_subject = "This is a test";
-//    function died($error) {
-//        // your error code can go here
-//        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-//        echo "These errors appear below.<br /><br />";
-//        echo $error."<br /><br />";
-//        echo "Please go back and fix these errors.<br /><br />";
-//        die();
-//    }
-//    // validation expected data exists
-//    if(!isset($_POST['first_name']) ||
-//        !isset($_POST['last_name']) ||
-//        !isset($_POST['email']) ||
-//        !isset($_POST['telephone']) ||
-//        !isset($_POST['comments'])) {
-//        died('We are sorry, but there appears to be a problem with the form you submitted.');
-//    }
-//    $first_name = $_POST['first_name']; // required
-//    $last_name = $_POST['last_name']; // required
-//    $email_from = $_POST['email']; // required
-//    $telephone = $_POST['telephone']; // not required
-//    $comments = $_POST['comments']; // required
-//    $error_message = "";
-//    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-//    if(!preg_match($email_exp,$email_from)) {
-//        $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-//    }
-//    $string_exp = "/^[A-Za-z .'-]+$/";
-//    if(!preg_match($string_exp,$first_name)) {
-//        $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-//    }
-//    if(!preg_match($string_exp,$last_name)) {
-//        $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-//    }
-//    if(strlen($comments) < 2) {
-//        $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-//    }
-//    if(strlen($error_message) > 0) {
-//        died($error_message);
-//    }
-//    $email_message = "Form details below.\n\n";
-//    function clean_string($string) {
-//        $bad = array("content-type","bcc:","to:","cc:","href");
-//        return str_replace($bad,"",$string);
-//    }
-//    $email_message .= "First Name: ".clean_string($first_name)."\n";
-//    $email_message .= "Last Name: ".clean_string($last_name)."\n";
-//    $email_message .= "Email: ".clean_string($email_from)."\n";
-//    $email_message .= "Telephone: ".clean_string($telephone)."\n";
-//    $email_message .= "Comments: ".clean_string($comments)."\n";
-//
-//// create email headers
-//    $headers = 'From: '.$email_from."\r\n".
-//        'Reply-To: '.$email_from."\r\n" .
-//        'X-Mailer: PHP/' . phpversion();
-//    @mail($email_to, $email_subject, $email_message, $headers);
-//    ?>
-<!--    <!-- include your own success html here -->-->
-<!--    Thank you for contacting us. We will be in touch with you very soon.-->
-<?php
-//}
-//?>
-<?php
+$sendgrid = new SendGrid('SENDGRID_PASSWORD', 'SENDGRID_USERNAME');
 
-$url = 'https://api.sendgrid.com/';
-$user = 'app35784872@heroku.com';
-$pass = 'jamesbond7';
-
-$params = array(
-    'api_user'  => $user,
-    'api_key'   => $pass,
-    'to'        => 'sanders271@gmail.com',
-    'subject'   => 'testing from curl',
-    'html'      => 'testing body',
-    'text'      => 'testing body',
-    'from'      => 'example@sendgrid.com',
-);
-
-
-$request =  $url.'api/mail.send.json';
-
-// Generate curl request
-$session = curl_init($request);
-// Tell curl to use HTTP POST
-curl_setopt ($session, CURLOPT_POST, true);
-// Tell curl that this is the body of the POST
-curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-// Tell curl not to return headers, but do return the response
-curl_setopt($session, CURLOPT_HEADER, false);
-// Tell PHP not to use SSLv3 (instead opting for TLS)
-curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-
-// obtain response
-$response = curl_exec($session);
-curl_close($session);
-
-// print everything out
-print_r($response);
-
+$message = new SendGrid\Email();
+$message->addTo('sanders271@gmail.com')->
+setFrom('sanders271@gmail.com')->
+setSubject('Subject goes here')->
+setText('Hello World!')->
+setHtml('<strong>Hello World!</strong>');
+$response = $sendgrid->send($message);
 ?>
