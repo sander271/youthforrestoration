f4cbb6c95f4883a26a6b5806a5671d8e59cd4e08
Aston Sanders <sanders271@gmail.com>
2015-04-27 16:40:45 -0400

    test email function

--- web/send_form_email.php
+++ web/send_form_email.php
@@ -1,6 +1,6 @@
 <?php
 require 'vendor/autoload.php';
-$sendgrid = new SendGrid('SENDGRID_PASSWORD', 'SENDGRID_USERNAME');
+$sendgrid = new SendGrid("$_ENV('SENDGRID_USERNAME')", "$_ENV('SENDGRID_PASSWORD')");
 
 $message = new SendGrid\Email();
 $message->addTo('sanders271@gmail.com')->
