71028ea3a41331de96a688820ee1623032fcfacb
Aston Sanders <sanders271@gmail.com>
2015-04-27 17:35:45 -0400

    test email function

--- web/send_form_email.php
+++ web/send_form_email.php
@@ -1,18 +1,18 @@
 <?php
-//require 'vendor/autoload.php';
-//echo "<h1>hello " .var_dump(getenv('SENDGRID_USERNAME'),$_ENV). "</h1>";
-//$SENDGRID_USERNAME = $_ENV["SENDGRID_USERNAME"];
-//$SENDGRID_PASSWORD = $_ENV["SENDGRID_PASSWORD"];
-//$sendgrid = new SendGrid($SENDGRID_USERNAME, $SENDGRID_PASSWORD);
-//
-//$message = new SendGrid\Email();
-//$message->addTo('sanders271@gmail.com')->
-//setFrom('sanders271@gmail.com')->
-//setSubject('Subject goes here')->
-//setText('Hello World!')->
-//setHtml('<strong>Hello World!</strong>');
-//$response = $sendgrid->send($message);
-//?>
+require 'vendor/autoload.php';
+$sendgrid = new SendGrid('app35784872@heroku.com', 'djlwgn2x');
+$email = new SendGrid\Email();
+$email
+    ->addTo('sanders271@gmail.com')
+    ->setFrom('me@bar.com')
+    ->setSubject('Subject goes here')
+    ->setText('Hello World!')
+    ->setHtml('<strong>Hello World!</strong>')
+;
+
+$sendgrid->send($email);
+
+?>
 <?php
 //// the message
 //$msg = "First line of text\nSecond line of text";
@@ -24,11 +24,11 @@
 //mail("someone@example.com","My subject",$msg);
 //?>
 <?php
-$to = "sanders271@gmail.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: webmaster@example.com" . "\r\n" .
-    "CC: somebodyelse@example.com";
-
-mail($to,$subject,$txt,$headers);
-?>
+//$to = "sanders271@gmail.com";
+//$subject = "My subject";
+//$txt = "Hello world!";
+//$headers = "From: webmaster@example.com" . "\r\n" .
+//    "CC: somebodyelse@example.com";
+//
+//mail($to,$subject,$txt,$headers);
+//?>
