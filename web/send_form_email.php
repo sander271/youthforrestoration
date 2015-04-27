e894a4b338b84453a60b1c14e0665d4f0c9bfd37
Aston Sanders <sanders271@gmail.com>
2015-04-27 17:25:52 -0400

    test email function

--- web/send_form_email.php
+++ web/send_form_email.php
@@ -1,15 +1,34 @@
 <?php
-require 'vendor/autoload.php';
-echo "<h1>hello " .var_dump(getenv('SENDGRID_USERNAME'),$_ENV). "</h1>";
-$SENDGRID_USERNAME = $_ENV["SENDGRID_USERNAME"];
-$SENDGRID_PASSWORD = $_ENV["SENDGRID_PASSWORD"];
-$sendgrid = new SendGrid($SENDGRID_USERNAME, $SENDGRID_PASSWORD);
+//require 'vendor/autoload.php';
+//echo "<h1>hello " .var_dump(getenv('SENDGRID_USERNAME'),$_ENV). "</h1>";
+//$SENDGRID_USERNAME = $_ENV["SENDGRID_USERNAME"];
+//$SENDGRID_PASSWORD = $_ENV["SENDGRID_PASSWORD"];
+//$sendgrid = new SendGrid($SENDGRID_USERNAME, $SENDGRID_PASSWORD);
+//
+//$message = new SendGrid\Email();
+//$message->addTo('sanders271@gmail.com')->
+//setFrom('sanders271@gmail.com')->
+//setSubject('Subject goes here')->
+//setText('Hello World!')->
+//setHtml('<strong>Hello World!</strong>');
+//$response = $sendgrid->send($message);
+//?>
+<?php
+//// the message
+//$msg = "First line of text\nSecond line of text";
+//
+//// use wordwrap() if lines are longer than 70 characters
+//$msg = wordwrap($msg,70);
+//
+//// send email
+//mail("someone@example.com","My subject",$msg);
+//?>
+<?php
+$to = "sanders271@gmail.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+    "CC: somebodyelse@example.com";
 
-$message = new SendGrid\Email();
-$message->addTo('sanders271@gmail.com')->
-setFrom('sanders271@gmail.com')->
-setSubject('Subject goes here')->
-setText('Hello World!')->
-setHtml('<strong>Hello World!</strong>');
-$response = $sendgrid->send($message);
+mail($to,$subject,$txt,$headers);
 ?>
