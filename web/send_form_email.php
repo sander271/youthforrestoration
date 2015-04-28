946fa99196f806c63ae48e69694f4af429b50d1f
Aston Sanders <sanders271@gmail.com>
2015-04-27 23:44:49 -0400

    add email function

--- web/send_form_email.php
+++ web/send_form_email.php
@@ -13,7 +13,7 @@
         <?php
         include 'vendor/autoload.php';
 
-        if(isset($_GET['email'])) {
+        if(isset($_POST['email'])) {
         // EDIT THE 2 LINES BELOW AS REQUIRED
             $email_to = "sanders271@gmail.com";
             $email_subject = "Message from Youth for Restoration Website";
@@ -28,19 +28,19 @@
             }
 
         // validation expected data exists
-            if (!isset($_GET['first_name']) ||
-                !isset($_GET['last_name']) ||
-                !isset($_GET['email']) ||
-                !isset($_GET['telephone']) ||
-                !isset($_GET['comments'])
+            if (!isset($_POST['first_name']) ||
+                !isset($_POST['last_name']) ||
+                !isset($_POST['email']) ||
+                !isset($_POST['telephone']) ||
+                !isset($_POST['comments'])
             ) {
                 died('We are sorry, but there appears to be a problem with the form you submitted.');
             }
-            $first_name = $_GET['first_name']; // required
-            $last_name = $_GET['last_name']; // required
-            $email_from = $_GET['email']; // required
-            $telephone = $_GET['telephone']; // not required
-            $comments = $_GET['comments']; // required
+            $first_name = $_POST['first_name']; // required
+            $last_name = $_POST['last_name']; // required
+            $email_from = $_POST['email']; // required
+            $telephone = $_POST['telephone']; // not required
+            $comments = $_POST['comments']; // required
             $error_message = "";
             $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
             if (!preg_match($email_exp, $email_from)) {
@@ -73,6 +73,8 @@
             $email_message .= "Comments: " . clean_string($comments) . "\n";
         // create email headers
             $headers = 'Reply-To: ' . $email_from . "\r\n";
+
+//            @mail($email_to, $email_subject, $email_message, $headers);
 
             $sendgrid = new SendGrid('app35784872@heroku.com', 'djlwgn2x',array("turn_off_ssl_verification" => true));
             $email = new SendGrid\Email();
